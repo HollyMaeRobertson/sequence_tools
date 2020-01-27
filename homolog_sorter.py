fa77255e155ba880478a41170c85df6abb0c321b
Holly <hmr42@cam.ac.uk>
2020-01-27 13:40:23 +0000

    updates to homolog_sorter

--- homolog_sorter.py
+++ homolog_sorter.py
@@ -2,7 +2,7 @@ import sys
 import os
 import blast_tools as blst
 
-#usage instructions 
+#usage instructions
 if len(sys.argv) != 3:
     print("This script compares sets of homologs and puts them with their " +
           "closest likely relatives." + "\nUsage: homolog_sorter.py " +
@@ -14,8 +14,7 @@ files_for_db = sys.argv[1]
 files_to_compare = sys.argv[2]
 db_file = str(sys.argv[1]) + "for_db"
 outW_db = open(db_file, 'w')
-cp_file = str(sys.argv[2]) + "_to_cp"
-outW_cp = open(cp_file, 'w')
+cp_dict = {}
 seq = ''
 
 #sequences to make database
@@ -63,6 +62,7 @@ for file in db_file_list:
 db_name = str(db_file) + "_database"
 blst.db_maker(db_file, db_name, "nucl")
 
+
 #read 10 of each from the next set of sequences
 compare_file_list = os.listdir(files_to_compare)
 
@@ -88,13 +88,11 @@ for file in compare_file_list:
                     
                 #all the other non-sequence lines
                 else:               
-                    #write sequence out to new file
-                    outW_cp.write(name)
-                    outW_cp.write(seq)
+                    #put sequence in dictionary
+                    cp_dict[name] = seq
                     
                     #re-initialise
                     name = line
-                    last_seq = seq
                     seq = ''
                     
             #add sequence to the string seq
@@ -102,14 +100,8 @@ for file in compare_file_list:
                 seq += line
                 
         #add the very last sequence to sequences
-        outW.write(name)
-        outW.write(seq)
+        cp_dict[name] = seq
         
-        #compare all sequences to database
-        output_name = str(file) + "_output"
-        #need to loop through all genes??
-        blst.blast_runner(gene, db_name, output_name)
-
-
-
-#put each file into the place it goes
+#compare all sequences to database
+##need to loop through all genes??
+##and put each file into the place it goes?
