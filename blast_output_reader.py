f4abe920fea5ee976ddaad52ab27b7ad14da395c
Holly Robertson <holly.robertson@slcu.cam.ac.uk>
2020-01-30 16:55:19 +0000

    changes to blast output reader

--- blast_output_reader.py
+++ blast_output_reader.py
@@ -1,15 +1,16 @@
 import sys
 import re
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
     print("This takes a tab-separated blast output file and " +
         "returns the genome locations where the genes are found" +
-        "\nUsage: blast_output_reader.py <blast_file> <genome_file>")
+        "\nUsage: blast_output_reader.py <blast_file> <genome_file> <flanking_seq_length>")
     sys.exit()
 
 #initialise things
 blast_file = sys.argv[1]
 genome_file = sys.argv[2]
+flank_len = int(sys.argv[3])
 gene_name = ''
 line_no = 0
 genes = {}
@@ -24,19 +25,30 @@ with open(blast_file) as f:
         
         #if we're still on the same gene
         if gene_name == blast_output[0]:
+            
             #as long as we're still in the same location
             if first_location == blast_output[1]:
-            
+                
+                #is gene in right orientation
+                if blast_output[8] < blast_output[9]:
+                    start = blast_output[8]
+                    end = blast_output[9]
+                else:
+                    start = blast_output[9]
+                    end = blast_output[8]
+                
                 #if the start is less than the current start, store it
-                if blast_output[8] < current_start:
-                    current_start = blast_output[8]
+                if start < current_start:
+                    current_start = start
                 #if the end is more than the current end, store it
-                if blast_output[9] > current_end:
-                    current_end = blast_output[9]
+                if end > current_end:
+                    current_end = end
             else:
                 continue
+                
         #if we're on a new gene
         else:
+            #for everything but the first line, store an entry
             if line_no != 1:
                 genes[gene_name] = {'start': current_start, 
                                     'end': current_end, 
@@ -83,7 +95,7 @@ with open(genome_file) as f:
                     print('>location in genome: ' + gene_id)
                     
                     #slice out the right bit of sequence
-                    seq = seq[start:end]
+                    seq = seq[start-flank_len:end+flank_len]
                     print(seq + '\n')
                 
             #re-initialise variables
