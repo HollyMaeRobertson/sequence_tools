fafbb638bcb154f631741cf9f22505d15de116b7
U-SLCU\holly.robertson <holly.robertson@SLPC402.slcu.private.cam.ac.uk>
2020-02-21 12:25:16 +0000

    updates to sequence_tools and blast_tools

--- blast_tools.py
+++ blast_tools.py
@@ -6,12 +6,61 @@ def db_maker(db, outfile, nt_or_aa):
     command = "makeblastdb -in " + db + " -out " + outfile + " -dbtype " + nt_or_aa
     os.system(command)
     
-def blast_runner(gene, db, outfile):
-    '''compares a query sequence to a database with blast'''
-    command = "blastn -query "
-    + str(gene) + " -db " 
-    + str(db)
-    + " -out "
-    + str(outfile)
-    + " -evalue 1e-3 -outfmt 6"
+def blast_runner(genes, db, outfile):
+    '''compares a file of query sequences to a database with blast'''
+    command = "blastn -query " + str(genes) + " -db " + str(db) + " -out " + str(outfile) + " -evalue 1e-3 -outfmt 6"
     os.system(command)
+    
+def blast_reader(blast_file, db_dict):
+    '''reads blast output and puts each gene with 
+    all the homolog groups it should go into'''
+    
+    #initialise
+    gene_name = ''
+    line_no = 0
+    i = 0
+    files = []
+    groups = []
+    gene_assigns = {}
+    
+    #open the blast output
+    with open(blast_file) as f:
+        for line in f:
+            #strip newlines, counter and turn line into a list
+            line = line.strip()
+            line_no += 1
+            blast_output = line.split('\t')
+            
+            #if we're still on the same gene
+            if gene_name == blast_output[0]:
+                
+                #if we're still on the same group
+                current_match = ">" + blast_output[1]
+                group = db_dict[current_match]
+                
+                if group in groups:
+                        continue
+                        
+                else:
+                    groups.append(group)
+                    
+                    
+            #if we're on a new gene
+            else:
+                #store everything
+                if line_no != 1:
+                    gene_assigns[gene] = groups
+                
+                #initialise variables
+                groups = []
+                i = 0
+                
+                #set match things
+                gene_name = blast_output[0]
+                gene = ">" + gene_name
+                match = ">" + blast_output[1]
+                groups.append(db_dict[match])
+                
+        gene_assigns[gene] = groups
+        
+    return gene_assigns 
--- homolog_sorter.py
+++ homolog_sorter.py
@@ -3,30 +3,35 @@ import os
 import blast_tools as blst
 
 #usage instructions
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
     print("This script compares sets of homologs and puts them with their " +
           "closest likely relatives." + "\nUsage: homolog_sorter.py " +
-          "set_of_sequencesA set_of_sequencesB")
+          "set_of_sequencesA set_of_sequencesB names_for_outfiles")
     sys.exit()
 
 #initialise things
 files_for_db = sys.argv[1]
 files_to_compare = sys.argv[2]
-db_file = str(sys.argv[1]) + "for_db"
+db_file = str(sys.argv[1]) + "_for_db"
 outW_db = open(db_file, 'w')
-cp_dict = {}
+cp_file = str(sys.argv[2]) + "_to_cp"
+outW_cp = open(cp_file, 'w')
+outfile_names = sys.argv[3]
 seq = ''
+db_dict = {}
+cp_dict = {}
 
 #sequences to make database
 db_file_list = os.listdir(files_for_db)
-
+ 
 for file in db_file_list:
+
     with open(files_for_db + "/" + file) as f:
         #initialise seqNumber
         seqNumber = 0
         
         for line in f:
-            line.strip()
+            line = line.strip()
             
             #sequence names all start with >
             if line[0] == ">":
@@ -42,8 +47,9 @@ for file in db_file_list:
                 #all the other non-sequence lines
                 else:               
                     #write sequence out to new file
-                    outW_db.write(name)
-                    outW_db.write(seq)
+                    outW_db.write(name + "\n")
+                    outW_db.write(seq + "\n")
+                    db_dict[name] = file
                     
                     #re-initialise
                     name = line
@@ -55,11 +61,12 @@ for file in db_file_list:
                 seq += line
                 
         #add the very last sequence to sequences
-        outW_db.write(name)
-        outW_db.write(seq)
+        outW_db.write(name + "\n")
+        outW_db.write(seq + "\n")
+        db_dict[name] = file
 
 #create a blast database from the sequences
-db_name = str(db_file) + "_database"
+db_name = str(files_for_db) + "_database"
 blst.db_maker(db_file, db_name, "nucl")
 
 
@@ -72,7 +79,7 @@ for file in compare_file_list:
         seqNumber = 0
         
         for line in f:
-            line.strip()
+            line = line.strip()
             
             #sequence names all start with >
             if line[0] == ">":
@@ -88,8 +95,10 @@ for file in compare_file_list:
                     
                 #all the other non-sequence lines
                 else:               
-                    #put sequence in dictionary
-                    cp_dict[name] = seq
+                    #put seq in file
+                    outW_cp.write(name + "\n")
+                    outW_cp.write(seq + "\n")
+                    cp_dict[name] = file
                     
                     #re-initialise
                     name = line
@@ -99,9 +108,132 @@ for file in compare_file_list:
             else:
                 seq += line
                 
-        #add the very last sequence to sequences
-        cp_dict[name] = seq
+        #add the very last sequence to file
+        outW_cp.write(name + "\n")
+        outW_cp.write(seq + "\n")
+        cp_dict[name] = file
+       
+
+#compare all sequences to database using blast
+blast_file = 'blast.txt'
+blst.blast_runner(cp_file, db_name, blast_file)
+
+#read the blast output and assign each gene a list of groups
+gene_assigns = blst.blast_reader(blast_file, db_dict)
+
+#want to make a list of lists 
+groups_list = []
+i = 0
+
+for key in gene_assigns.keys():
+    
+    gene_group = cp_dict[key]
+    
+    #set up first list
+    if i == 0:
+        groups_list.append([])
+        groups_list[i].append(gene_group)
         
-#compare all sequences to database
-##need to loop through all genes??
-##and put each file into the place it goes?
+        for j in gene_assigns[key]:
+            groups_list[i].append(j)
+    
+    #all other entries
+    else:
+        finished = False
+    
+        #loop through the list of groups
+        for this_list in groups_list:
+            
+            #if the gene group is in this entry
+            if gene_group in this_list:
+                
+                #we're done with this gene group because we can add all the entries
+                finished = True
+                
+                #add all the new entries
+                for group in gene_assigns[key]:
+                    if group in this_list:
+                        continue
+                    else:
+                        this_list.append(group)
+                        
+            #if the gene group is not in this entry
+            else:
+                continue #it might be in another entry further on in the list
+        
+        
+        #outside the for loop
+        #if we never found an entry with gene group in
+        if finished:
+            continue   
+        else:
+            #loop through groups list
+            for this_list in groups_list:
+        
+                #looping through the gene assigns
+                for group in gene_assigns[key]:
+                
+                    #if one of the gene assigns is in the groups list already
+                    if group in this_list:
+                        
+                        #we're done with this group
+                        finished = True
+                        
+                        #add all the new entries
+                        #add the gene group (once)
+                        if gene_group in this_list:
+                            continue
+                        else:
+                            this_list.append(gene_group)
+                        
+                        for x in gene_assigns[key]:
+                            if x in this_list:
+                                continue
+                            else:
+                                this_list.append(x)
+                        
+                    else:
+                        continue
+        
+        #if we never found an entry with any of the gene assigns in it
+        if finished:
+            continue
+        else: 
+            #make a new list and add all the entries 
+            groups_list.append([])
+            groups_list[i].append(gene_group)
+            
+            for j in gene_assigns[key]:
+                groups_list[i].append(j)
+                
+    i += 1
+
+  
+output_file = outfile_names + "_output"
+outW_output = open(output_file, 'w')
+i = 0
+
+for entry in groups_list:
+    
+    i += 1
+    sequences = ''
+    
+    outW_output.write("\n\n>" + outfile_names + "_" + str(i))
+    
+    for group in entry:
+        if group in db_file_list:   
+            file = open(files_for_db + "/" + group)
+        else:
+            file = open(files_to_compare + "/" + group)
+        
+        with file as f:
+            for line in f:
+                sequences += line
+                
+        outW_output.write("\n" + group)
+    
+    file_name = outfile_names + "_" + str(i)
+    outW_final = open(file_name, 'w')
+    
+    outW_final.write(sequences)
+    
